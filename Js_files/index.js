368ef0e76e4ddd02b80dc65f46d9070b43a6e192
Vaibhav Badiger <badigervaibhav8@gmail.com>
2021-07-14 17:03:41 +0530

    added files

---
+++ Js_files/index.js
@@ -0,0 +1,7 @@
+let navbar = document.querySelector('.navbar');
+let hamBurgMenu = document.querySelector('.hamBurgMenu');
+
+hamBurgMenu.addEventListener('click', ()=>{
+   navbar.classList.toggle('resp-nav')
+   
+})
